13943163346104daf9754c2a19417ffef6ac50db
Marko Dupor <marko.dupor@asseco-see.hr>
2021-08-09 10:15:11 +0200

    Minor

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Asseco\Attachments\Tests;
 
-use Asseco\BlueprintAudit\BlueprintAuditServiceProvider;
+use Asseco\Common\CommonServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
@@ -15,7 +15,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getPackageProviders($app): array
     {
-        return [BlueprintAuditServiceProvider::class];
+        return [CommonServiceProvider::class];
     }
 
     protected function getEnvironmentSetUp($app)
