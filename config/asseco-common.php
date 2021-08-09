026014b69bc01cf2bf06b59de39a0bdbac719564
Marko Dupor <marko.dupor@asseco-see.hr>
2021-08-09 13:11:47 +0200

    Removed unneeded stuff

--- config/asseco-common.php
+++ config/asseco-common.php
@@ -1,24 +1,5 @@
 <?php
 
-use Asseco\Common\App\Decorators\Audit;
-use Asseco\Common\App\Decorators\SoftDelete;
-use Asseco\Common\App\Decorators\SoftDeleteAudit;
-use Asseco\Common\App\Decorators\Uuid;
-use Asseco\Common\App\MigrationMethodPicker;
-
 return [
 
-    'uuid_decorator'       => Uuid::class,
-
-    /**
-     * Different decorators for different migration timestamp methods.
-     *
-     * @see MigrationMethodPicker
-     * @see https://github.com/asseco-voice/laravel-blueprint-audit
-     */
-    'migration_decorators' => [
-        MigrationMethodPicker::SOFT    => SoftDelete::class,
-        MigrationMethodPicker::PARTIAL => Audit::class,
-        MigrationMethodPicker::FULL    => SoftDeleteAudit::class,
-    ],
 ];
