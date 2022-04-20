1430a3341bbad3ae4296805508da226d97e75d04
Josip Milotić <42002911+josip-milotic@users.noreply.github.com>
2022-04-20 12:57:54 +0200

    Sqlite fix #patch (#1)
    * Fix for dropping foreign keyes in SQLite
    
    * Apply fixes from StyleCI
    
    * Comment
    
    * Lock revert
    
    Co-authored-by: jmilotic <josip.milotic@asseco-see.hr>
    Co-authored-by: StyleCI Bot <bot@styleci.io>
    Co-authored-by: Marko Dupor <marko.dupor@asseco-see.hr>

--- src/CommonServiceProvider.php
+++ src/CommonServiceProvider.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Asseco\Common;
 
+use Closure;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Support\Fluent;
 use Illuminate\Support\ServiceProvider;
 
 class CommonServiceProvider extends ServiceProvider
@@ -24,5 +29,42 @@ class CommonServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/asseco-common.php' => config_path('asseco-common.php'),
         ], 'asseco-common');
+
+        $this->hotfixSqlite();
+    }
+
+    /**
+     * Fix because SQLite throws an error if drop foreign is used in migrations.
+     *
+     * @return void
+     */
+    public function hotfixSqlite()
+    {
+        \Illuminate\Database\Connection::resolverFor('sqlite',
+            function ($connection, $database, $prefix, $config) {
+                return new class($connection, $database, $prefix, $config) extends SQLiteConnection
+                {
+                    public function getSchemaBuilder()
+                    {
+                        if ($this->schemaGrammar === null) {
+                            $this->useDefaultSchemaGrammar();
+                        }
+
+                        return new class($this) extends SQLiteBuilder
+                        {
+                            protected function createBlueprint($table, Closure $callback = null)
+                            {
+                                return new class($table, $callback) extends Blueprint
+                                {
+                                    public function dropForeign($index)
+                                    {
+                                        return new Fluent();
+                                    }
+                                };
+                            }
+                        };
+                    }
+                };
+            });
     }
 }
